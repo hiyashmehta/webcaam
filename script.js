496a1acc3125fe5bcb9c270b500cc256a3299ddf
Yash Mehta <yashmehta0395@gmail.com>
2023-09-25 20:45:22 +0530

    added image capture property

--- script.js
+++ script.js
@@ -1,41 +1,41 @@
-let video = document.querySelector("video");
-let recordBtnCont = document.querySelector(".record-btn-cont");
-let recordBtn = document.querySelector(".record-btn");
-let captureBtnCont = document.querySelector(".capture-btn-cont");
-let captureBtn = document.querySelector(".capture-btn");
-let recordFlag = false;
+//let video = document.querySelector("video");
+//let recordBtnCont = document.querySelector(".record-btn-cont");
+//let recordBtn = document.querySelector(".record-btn");
+//let captureBtnCont = document.querySelector(".capture-btn-cont");
+//let captureBtn = document.querySelector(".capture-btn");
+//let recordFlag = false;
 
-let recorder;
-let chunks = []; // Media data in chunks
+//let recorder;
+//let chunks = []; // Media data in chunks
 
-let constraints = {
-    video: true,
-    audio: true
-}
+//let constraints = {
+//    video: true,
+//    audio: true
+//}
 
 // navigator -> global browser info
-navigator.mediaDevices.getUserMedia(constraints)
-.then((stream) => {
-    video.srcObject = stream;
+//navigator.mediaDevices.getUserMedia(constraints)
+//.then((stream) => {
+//    video.srcObject = stream;
 
-    recorder = new MediaRecorder(stream);
-    recorder.addEventListener("start", (e) => {
-        chunks = [];
-    })
-    recorder.addEventListener("dataavailable", (e) => {
-        chunks.push(e.data);
-    })
-    recorder.addEventListener("click", (e) => {
+//    recorder = new MediaRecorder(stream);
+//    recorder.addEventListener("start", (e) => {
+//        chunks = [];
+//    })
+//    recorder.addEventListener("dataavailable", (e) => {
+//        chunks.push(e.data);
+//    })
+//    recorder.addEventListener("click", (e) => {
         // Conversion of media chunks data to video
-        let blob = new Blob(chunks, { type: "video/mp4" });
-        let videoURL = URL.createObjectURL(blob);
-
-        let a = document.createElement("a");
-        a.href = videoURL;
-        a.download = "stream.mp4";
-        a.click();
-    })
-})
+//        let blob = new Blob(chunks, { type: "video/mp4" });
+//        let videoURL = URL.createObjectURL(blob);
+//
+//        let a = document.createElement("a");
+//        a.href = videoURL;
+//        a.download = "stream.mp4";
+//        a.click();
+//    })
+//})
 
 recordBtnCont.addEventListener("click", (e) => {
     if (!recorder) return;
@@ -52,6 +52,21 @@ recordBtnCont.addEventListener("click", (e) => {
         recordBtn.classList.remove("scale-record");
         stopTimer();
     }
+})
+
+captureBtnCont.addEventListener("click", (e) => {
+    let canvas = document.createElement("canvas");
+    canvas.width = video.videoWidth;
+    canvas.height = video.videoHeight;
+
+    let tool = canvas.getContext("2d");
+    tool.drawImage(video, 0, 0, canvas.width, canvas.height);
+
+    let imageURL = canvas.toDataURL("image/jpg");
+    let a = document.createElement("a");
+    a.href = imageURL;
+    a.download = "image.jpg";
+    a.click();
 })
 
 let timerID;
