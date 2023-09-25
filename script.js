cd8ea0130170fa5303efdb7afc20c34311e1d5e9
Yash Mehta <yashmehta0395@gmail.com>
2023-09-25 22:03:05 +0530

    added filter property for video and image

--- script.js
+++ script.js
@@ -1,41 +1,42 @@
-//let video = document.querySelector("video");
-//let recordBtnCont = document.querySelector(".record-btn-cont");
-//let recordBtn = document.querySelector(".record-btn");
-//let captureBtnCont = document.querySelector(".capture-btn-cont");
-//let captureBtn = document.querySelector(".capture-btn");
-//let recordFlag = false;
+let video = document.querySelector("video");
+let recordBtnCont = document.querySelector(".record-btn-cont");
+let recordBtn = document.querySelector(".record-btn");
+let captureBtnCont = document.querySelector(".capture-btn-cont");
+let captureBtn = document.querySelector(".capture-btn");
+let recordFlag = false;
+let transparentColor = "transparent";
 
-//let recorder;
-//let chunks = []; // Media data in chunks
+let recorder;
+let chunks = []; // Media data in chunks
 
-//let constraints = {
-//    video: true,
-//    audio: true
-//}
+let constraints = {
+    video: true,
+    audio: true
+}
 
 // navigator -> global browser info
-//navigator.mediaDevices.getUserMedia(constraints)
-//.then((stream) => {
-//    video.srcObject = stream;
+navigator.mediaDevices.getUserMedia(constraints)
+.then((stream) => {
+    video.srcObject = stream;
 
-//    recorder = new MediaRecorder(stream);
-//    recorder.addEventListener("start", (e) => {
-//        chunks = [];
-//    })
-//    recorder.addEventListener("dataavailable", (e) => {
-//        chunks.push(e.data);
-//    })
-//    recorder.addEventListener("click", (e) => {
+    recorder = new MediaRecorder(stream);
+    recorder.addEventListener("start", (e) => {
+        chunks = [];
+    })
+    recorder.addEventListener("dataavailable", (e) => {
+        chunks.push(e.data);
+    })
+    recorder.addEventListener("click", (e) => {
         // Conversion of media chunks data to video
-//        let blob = new Blob(chunks, { type: "video/mp4" });
-//        let videoURL = URL.createObjectURL(blob);
-//
-//        let a = document.createElement("a");
-//        a.href = videoURL;
-//        a.download = "stream.mp4";
-//        a.click();
-//    })
-//})
+        let blob = new Blob(chunks, { type: "video/mp4" });
+        let videoURL = URL.createObjectURL(blob);
+
+        let a = document.createElement("a");
+        a.href = videoURL;
+        a.download = "stream.mp4";
+        a.click();
+    })
+})
 
 recordBtnCont.addEventListener("click", (e) => {
     if (!recorder) return;
@@ -61,8 +62,12 @@ captureBtnCont.addEventListener("click", (e) => {
 
     let tool = canvas.getContext("2d");
     tool.drawImage(video, 0, 0, canvas.width, canvas.height);
+    // filtering
+    tool.fillStyle = transparentColor;
+    tool.fillRect(0, 0, canvas.width, canvas.height);
 
     let imageURL = canvas.toDataURL("image/jpg");
+
     let a = document.createElement("a");
     a.href = imageURL;
     a.download = "image.jpg";
@@ -100,3 +105,16 @@ function stopTimer() {
     timer.innerText = "00:00:00";
     timer.style.display = "none";
 }
+
+
+
+// Filtering Logic
+let filterLayer = document.querySelector(".filter-layer");
+let allFilters = document.querySelectorAll(".filter");
+allFilters.forEach((filterElem) => {
+    filterElem.addEventListener("click", (e) => {
+        // Get style
+        transparentColor = getComputedStyle(filterElem).getPropertyValue("background-color");
+        filterLayer.style.backgroundColor = transparentColor;
+    })
+})
